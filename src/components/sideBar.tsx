5c67b8d89afe8e47eef71dffb67b8c16ba1d0426
FiuzaDevs <pedrofiuza1@gmail.com>
2021-09-10 13:09:06 -0300

    main

---
+++ src/components/sideBar.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { FaCogs } from "react-icons/fa";
+import { useHistory } from "react-router-dom";
+
+export default function Sidebar() {
+  let history = useHistory();
+
+  function handleExit() {
+    history.push("/");
+  }
+  function handleClick() {
+    history.push("/Configuracao");
+  }
+
+  return (
+    <aside>
+      <header>
+        <h2>Username</h2>
+        <button onClick={handleClick}>
+          <img
+            alt="userImg"
+            src="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+            width="55"
+            height="66"
+          />
+        </button>
+      </header>
+
+      <footer>
+        <button type="button" onClick={handleClick}>
+          <FaCogs size={46} />
+        </button>
+        <button type="button" onClick={handleExit}>
+          Sair
+        </button>
+      </footer>
+    </aside>
+  );
+}
